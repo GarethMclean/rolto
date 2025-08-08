14ab1dbb360cfd3e1c847c67ece8cd17596a6931
GarethMclean <74466663+GarethMclean@users.noreply.github.com>
2025-08-08 14:40:32 -0500

    Fix middleware matcher to properly exclude API routes from authentication

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { auth } from "@/auth"
 import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth
@@ -33,5 +32,15 @@ export default auth((req) => {
 })
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
+  ],
 }
